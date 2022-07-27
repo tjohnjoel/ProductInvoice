91a6545d18d52b2e7d55ae31dc2e650f65a9d490
John Joel Thetla <hjx1kor@bosch.com>
2022-07-27 12:58:06 +0530

    Changes

--- ProductInvoice/Models/Discount.cs
+++ ProductInvoice/Models/Discount.cs
@@ -1,11 +1,18 @@
-﻿namespace ProductInvoice.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductInvoice.Models
 {
     public class Discount
     {
         public Guid DiscountId { get; set; }
         public Guid ProductId { get; set; }
+        [Required]
+        [DisplayName("Percentage")]
         public int DiscountPercent { get; set; }
         public DateTime DiscountCreated { get; set; }
+        [Required]
+        [DisplayName("Expiration Date")]
         public DateTime? DiscountExpiration { get; set; }
     }
 }
